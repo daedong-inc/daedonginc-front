5dd4ba30fafe9fa2643e310cc8281c8ebda5d1c0
은정 <scpark6021@gmail.com>
2023-08-14 15:11:52 +0900

    [Feat]: theme(colors, mediaQuery)수정

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,33 @@
 export interface Theme {
   colors: {
-    black: string;
     white: string;
-    gray1: string; //unpicked button
-    gray2: string; //more button
-    gray3: string; //화살표 슬라이드 버튼
-    background: string; //이미지 없음 배경
+    black: string;
+    // gray
+    gray25: string;
+    gray50: string;
+    gray100: string;
+    gray200: string;
+    gray300: string;
+    gray400: string;
+    gray500: string;
+    gray600: string;
+    gray700: string;
+    gray800: string;
+    gray900: string;
+    gray950: string;
+    // neutrals
+    neutral25: string;
+    neutral50: string;
+    neutral100: string;
+    neutral200: string;
+    neutral300: string;
+    neutral400: string;
+    neutral500: string;
+    neutral600: string;
+    neutral700: string;
+    neutral800: string;
+    neutral900: string;
+    neutral950: string;
   };
   media: {
     desktop_L: string;
@@ -16,12 +38,34 @@ export interface Theme {
 
 const theme: Theme = {
   colors: {
+    white: '#FFFFFF',
     black: '#000000',
-    white: '#ffffff',
-    gray1: '#666666',
-    gray2: '#BDBDBD',
-    gray3: '#D9D9D9',
-    background: '#F6F6F6',
+    // gray
+    gray25: '#979AA0',
+    gray50: '#84898F',
+    gray100: '#777C81',
+    gray200: '#666C73',
+    gray300: '#5B6066',
+    gray400: '#4D5256',
+    gray500: '#45484B',
+    gray600: '#3A3C3E',
+    gray700: '#343537',
+    gray800: '#2B2D2F',
+    gray900: '#252525',
+    gray950: '#181818',
+    // neutrals
+    neutral25: '#FCFCFD',
+    neutral50: '#FAFBFC',
+    neutral100: '#F8F9FB',
+    neutral200: '#F9F9F9',
+    neutral300: '#F7F7F8',
+    neutral400: '#F5F5F5',
+    neutral500: '#EFEFF1',
+    neutral600: '#EAEBF0',
+    neutral700: '#E5E5E7',
+    neutral800: '#C1C3C7',
+    neutral900: '#A1A4AC',
+    neutral950: '#9599A1',
   },
   media: {
     desktop_L: '@media (min-width: 1025px)',
