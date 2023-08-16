ec6f186b221528bd53e26bb5c9d2eeb510c68670
은정 <scpark6021@gmail.com>
2023-08-16 16:59:43 +0900

    [Feat]: common button component

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,62 @@
+import { forwardRef } from 'react';
+import { ReactComponent as ButtonArrow } from 'assets/icons/buttonArrow.svg';
+
+import * as S from './Button.style';
+
+export type ButtonSize = 'l' | 'm' | 's';
+export type ButtonState = 'enabled' | 'hovered' | 'disabled';
+export type ButtonType = 'icon' | 'text' | 'text_icons';
+
+export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+  as?: keyof JSX.IntrinsicElements;
+  outlined?: boolean;
+  size?: ButtonSize;
+  state?: ButtonState;
+  buttontype?: ButtonType;
+}
+
+const Button = forwardRef<HTMLButtonElement, Props>(function Button(
+  {
+    children,
+    as = 'button',
+    outlined = false,
+    size = 's',
+    state = 'enabled',
+    buttontype = 'text',
+    onClick,
+    ...rest
+  }: Props,
+  ref,
+) {
+  let content: React.ReactNode = children;
+
+  if (buttontype === 'icon') {
+    content = <ButtonArrow />;
+  } else if (buttontype === 'text_icons') {
+    content = (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {children}
+        <ButtonArrow style={{ marginLeft: '8px' }} />
+      </div>
+    );
+  } else {
+    content = children;
+  }
+
+  return (
+    <S.Container
+      as={as}
+      outlined={outlined}
+      size={size}
+      state={state}
+      buttontype={buttontype}
+      ref={ref}
+      {...rest}
+    >
+      {content}
+    </S.Container>
+  );
+});
+
+export default Button;
