7f271504ef3fb53be245363ca0ae42272638cb8b
은정 <scpark6021@gmail.com>
2023-08-22 13:59:40 +0900

    [Feat]: scrollTop floating button 추가

---
+++ src/components/common/Button/FloatingButton.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as ScrollTopIcon } from '@assets/icons/scrollTop.svg';
+import theme from 'theme';
+
+interface StyledButtonProps {
+  isVisible: boolean;
+}
+
+const FloatingButton: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const isScrolledUp = currentScrollY < prevScrollY;
+      const isAtTop = currentScrollY <= 200;
+      const isAtBottom =
+        currentScrollY + window.innerHeight >= document.body.scrollHeight;
+
+      setIsVisible((isScrolledUp && !isAtTop) || isAtBottom);
+      setPrevScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollY]);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  return (
+    <StyledButton isVisible={isVisible} onClick={scrollToTop}>
+      <ScrollTopIcon />
+    </StyledButton>
+  );
+};
+
+export default FloatingButton;
+
+const StyledButton = styled.div<StyledButtonProps>`
+  position: fixed;
+  bottom: 10%;
+  right: calc(15vw / 2);
+  background: none;
+  border-radius: 5px;
+  cursor: pointer;
+  z-index: 10;
+  display: ${({ isVisible }) => (isVisible ? 'block' : 'none')};
+`;
