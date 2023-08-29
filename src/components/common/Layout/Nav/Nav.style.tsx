58f065b749bd8a1db95b31850bbce83e507a3b03
박 은정 <scpark6021@gmail.com>
2023-08-29 20:58:53 +0900

    [Feat]: nav검색바 추가

--- src/components/common/Layout/Nav/Nav.style.tsx
+++ src/components/common/Layout/Nav/Nav.style.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { Z_INDEX } from 'styles/constants';
 import { getNotoTypographyStyles } from 'styles/font/font';
-import { mediaQuery } from 'styles/mediaQuery';
 import theme from 'theme';
+import { ReactComponent as Icon } from 'assets/icons/search.svg';
 
 export const TopNavContainer = styled.div`
   position: fixed;
@@ -43,9 +43,50 @@ export const NavMenu = styled.ul`
   color: ${theme.colors.white};
 `;
 
+// 검색
+
 export const SearchWrapper = styled.div`
-  cursor: pointer;
-  flex: 1;
   display: flex;
-  justifycontent: flex-end;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 100px;
+  padding: 0px 16px;
+  width: 220px;
+  height: 34px;
+
+  ${({ theme }) => theme.media.desktop_S} {
+    background: none;
+    border: none;
+    width: 20px;
+    margin-left: 20px;
+  }
+`;
+
+export const SearchIcon = styled(Icon)`
+  font-size: 15px;
+  cursor: pointer;
+`;
+
+export const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  padding: 6px 12px;
+  font-size: 14px;
+  background-color: transparent;
+  ${getNotoTypographyStyles('Body3_r')}
+  color: white;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  ${({ theme }) => theme.media.desktop_S} {
+    display: none;
+  }
 `;
