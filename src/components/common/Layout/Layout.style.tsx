a17dd4b6dd23c3344fe8b2d9d33c56636ad75c88
은정 <scpark6021@gmail.com>
2023-09-15 19:51:55 +0900

    [Feat]: nav_mobile 반응형 mediaquery추가

--- src/components/common/Layout/Layout.style.tsx
+++ src/components/common/Layout/Layout.style.tsx
@@ -4,6 +4,12 @@ export const NavContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  /* height: 70px;
+
+  ${({ theme }) => theme.media.mobile} {
+    width: 90%;
+    height: 50px;
+  } */
 `;
 
 export const Container = styled.main`
